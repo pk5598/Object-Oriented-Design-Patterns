10b9f7bc5a2b5a6e8655b992e11a65bf179ace05
user <user@DESKTOP-04F52SO>
2022-06-09 15:56:18 +0530

    first commit

---
+++ Builder/src/com/company/Builders/HouseBuilder.java
@@ -0,0 +1,18 @@
+package com.company.Builders;
+
+import com.company.Product.House;
+
+
+public interface HouseBuilder {
+
+    public void buildBasement();
+
+    public void buildStructure();
+
+    public void buildRoof();
+
+    public void buildInterior();
+
+    public House getHouse();
+    
+}
